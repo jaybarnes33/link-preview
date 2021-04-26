627369e3f5c6b5e471efae984cde878c17bb5ce8
Oduro T.J Barnes <ohenesetwumasi@gmail.com>
2021-04-26 22:31:38 +0000

    Add image upload

--- src/pages/api/uploads/index.ts
+++ src/pages/api/uploads/index.ts
@@ -11,7 +11,7 @@ const router = nextConnect();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "./uploads/");
+    cb(null, "./public/uploads/");
   },
   filename(req, file, cb) {
     cb(
