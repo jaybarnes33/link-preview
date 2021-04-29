6dd8071ed746cad28708adf26d483c97355a61be
Oduro T.J Barnes <ohenesetwumasi@gmail.com>
2021-04-29 18:14:58 +0000

    fix bug

--- src/utils/get-userID.ts
+++ src/utils/get-userID.ts
@@ -2,7 +2,7 @@ import { verify } from "jsonwebtoken";
 
 const getUserID = (token: string) => {
   try {
-    const payload: any = verify(token, process.env.NEXT_PUBLIC_JWT_SECRET);
+    const payload: any = verify(token, process.env.JWT_SECRET);
     return payload.sub;
   } catch (error) {
     return error.message;
