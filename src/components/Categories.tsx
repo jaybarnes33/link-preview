0bc693edc70cbf4513e2d57c41ef624555f7a1cf
John Barnes <ohenesetwumasi@gmail.com>
2021-05-22 09:11:30 +0000

    Add fetch all

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -2,17 +2,17 @@ import makeSecuredRequest from "@/utils/makeSecuredRequest";
 import useSWR from "swr";
 import { Nav, NavItem } from "react-bootstrap";
 import Link from "next/link";
+import { useEffect } from "react";
 
 const Categories = () => {
   const fetchData = async () =>
     await makeSecuredRequest("/api/cards/categories", "GET");
   const { data } = useSWR(`/api/cards/categories`, fetchData); // useSWR for caching and realtime mutations
 
-  console.log(data);
   return (
     <Nav className="category-nav">
       <ul className="nav-links mx-auto">
-        {data?.map((category) => (
+        {["all", ...data]?.map((category) => (
           <span className="ml-2 px-2">
             <i className="bi bi-pin mr-2"></i>
             <Nav.Item
@@ -36,12 +36,13 @@ const Categories = () => {
           }
 
           span {
-            color:#f4f4f4;
+            color:red;
+ 
           }
          
-         .category-link{
-          color:white;
-         }
+       
+
+    
         `}
       </style>
     </Nav>
